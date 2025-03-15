41907b3ab0f55d27775b33908883a35bbf4ea266
Abdulatif <abdulatifsh90@gmail.com>
2025-03-16 04:26:54 +0900

    feat:  develop member scheme model

--- src/libs/enums/members.enum.ts
+++ src/libs/enums/members.enum.ts
@@ -1,3 +1,12 @@
+/* enum - bu u turli qiymatlarni birlashtirish uchun ishlatiladigan 
+ TypeScript xususiyatidir, bu orqali o'zgaruvchilarga aniq va nomlangan qiymatlar tayinlash mumkin.*/
+//  MemberType va MemberStatus enumlarini yaratamiz
+//  MemberType enumi USER va RESTARAUNT qiymatlarini o'z ichiga oladi
+//  MemberStatus enumi ACTIVE, BLOCK va DELETE qiymatlarini o'z ichiga oladi
+//  MemberType va MemberStatus enumlarini export qilamiz
+//  MemberType va MemberStatus enumlarini ishlatish
+//  MemberType va MemberStatus enumlarini ishlatish uchun Member.model.ts faylini o'zgartiramiz
+
 export enum MemberType {
     USER = "USER",
     RESTARAUNT = "RESTARAUNT"
@@ -8,3 +17,4 @@ export enum MemberStatus {
     BLOCK = "BLOCK",
     DELETE = "DELETE"
 }
+
--- src/schema/Member.model.ts
+++ src/schema/Member.model.ts
@@ -1,33 +1,40 @@
+// Desc: Member Schema for MongoDB
+// schema - bu MongoDB yoki boshqa NoSQL bazasida ma'lumotlar tuzilmasini aniqlovchi ma'lumotlar turi.
 import mongoose, { Schema } from "mongoose";
 import { MemberStatus, MemberType } from "../libs/enums/members.enum";
 
 // Build type: Schema First vs Code First
 const memberSchema = new Schema(
+  //memberType - foydalanuvchi turi, RESTARAUNT yoki USER bo'lishi mumkin, uning turi String va default qiymati USER
   {
     memberType: {
       type: String,
       enum: MemberType,
       default: MemberType.USER,
     },
-
+// memberStatus - foydalanuvchi holati, ACTIVE, BLOCK yoki DELETE bo'lishi mumkin, uning turi String va default qiymati ACTIVE
     memberStatus: {
       type: String,
       enum: MemberStatus,
       default: MemberStatus.ACTIVE,
     },
 
+ // memberNick - foydalanuvchi nomi, String turi va unique bo'lishi kerak,index: { unique: true, sparse: true }
+ // unique: true - bu maydonning unikal bo'lishi kerakligini bildiradi
+ // sparse: true - bu maydonning bo'sh bo'lishiga ruxsat beradi
+ // required: true - bu talab qiladiki, bu maydonni to'ldirish shart
     memberNick: {
       type: String,
       index: { unique: true, sparse: true },
       required: true,
     },
-
+//memberPhone - foydalanuvchi telefon raqami, String turi va unique bo'lishi kerak,index: { unique: true, sparse: true }
     memberPhone: {
       type: String,
       index: { unique: true, sparse: true },
       required: true,
     },
-
+//memberPassword - foydalanuvchi paroli, String turi va select: false, required: true, bu maydonni ko'rishni yopadi
     memberPassword: {
       type: String,
       select: false,
