30475fbf7a158e532dc7042b482e810d889dd2fd
Abdulatif <abdulatifsh90@gmail.com>
2025-03-21 18:15:17 +0900

    fixed data error

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,7 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/members.enum";
-import * as bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 class MemberService {
     static processLogin(newMember: MemberInput) {
@@ -17,45 +17,61 @@ class MemberService {
         this.memberModel = MemberModel;
     }
 
+
     public async processSignup(input: MemberInput): Promise<Member> {
+
         const exist = await this.memberModel
             .findOne({ memberType: MemberType.RESTARAUNT })
             .exec();
         
-        if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        if (exist) {
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
 
-        const salt = await bcrypt.genSalt()
-        input.memberPassword = await bcrypt.hash(input.memberPassword,salt)
-
+        const salt = await bcrypt.genSalt(10);
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
 
         try {
+         
             const result = await this.memberModel.create(input);
+
+           
             result.memberPassword = "";
+
             return result.toObject() as Member;
         } catch (err) {
+            console.error("Signup Error:", err);
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
     }
 
+    // Tizimga kirish (Login)
     public async processLogin(input: LoginInput): Promise<Member> {
-        const member = await this.memberModel
-            .findOne({ memberNick: input.memberNick })
-            .select("memberPassword") 
-            .exec();
-    
-        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-    
-        const isMatch = await bcrypt.compare(input.memberPassword,member.memberPassword)
-        if (!isMatch) {
-            throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+        try {
+       
+            const member = await this.memberModel
+                .findOne({ memberNick: input.memberNick })
+                .select("+memberPassword") 
+                .exec();
+
+            if (!member) {
+                throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+            }
+
+            const isMatch = await bcrypt.compare(input.memberPassword, member.memberPassword);
+            if (!isMatch) {
+                throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+            }
+
+         
+            member.memberPassword = "";
+
+            return member.toObject() as Member;
+        } catch (err) {
+            console.error("Login Error:", err);
+            throw err;
         }
-    
-     
-        member.memberPassword = "";
-    
-        return member.toObject() as Member;
     }
-    
 }
 
 export default MemberService;
