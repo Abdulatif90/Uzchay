0023abb1942f0cfbe88ff083ecf0e7cbadd9acd2
Abdulatif <abdulatifsh90@gmail.com>
2025-05-01 18:29:47 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,31 +4,19 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
-
-
-
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
+  
+const app = express()
 
 const MongoDBStore = ConnectMongoDB(session);
-const app = express();
+
 const store = new MongoDBStore({
-  uri: String(process.env.MONGODB_URL),
+  uri: String(process.env.MONGO_URL),
   collection:"sessions",
 });
 
 
-app.use(
-  session({
-    secret: String(process.env.SESSION_SECRET),
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 3 // 3 soat  - bu cookie ning muddati yani datalar saqlash turib muddati 
-    },
-    store: store,
-    resave: true,
-    saveUninitialized: true,  // false - birinchi tashrifda datalarni saqlash vaqti , agar true bo'lsa har safar saqlaydi 
-    })    
-);
 /** 1-Entrance **/
 
 app.use(express.static(path.join(__dirname,"public")));
@@ -36,6 +24,18 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT))
 /** 2-Sessions **/
+
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET), // bu secret key, uni o'zgartirmaslik kerak
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 3 // 3 soat  - bu cookie ning muddati yani datalar saqlash turib muddati 
+    },
+    store: store, // MongoDB ga saqlash uchun
+    resave: true, // true - har safar saqlaydi, false - faqat birinchi safar saqlaydi
+    saveUninitialized: true,  // false - birinchi tashrifda datalarni saqlash vaqti , agar true bo'lsa har safar saqlaydi 
+    })    
+);
 
 /** 3-Views **/
 app.set("views", path.join(__dirname, "views"))
