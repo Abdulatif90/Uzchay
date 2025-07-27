1c293d694a32c22017437f5ba18647f6025891ef
Abdulatif <abdulatifsh90@gmail.com>
2025-07-28 06:47:52 +0900

    feat: develop getMyOrders rest Api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -4,6 +4,7 @@ import { Response } from "express";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import OrderService from "../models/Order.service";
 import { OrderStatus } from "../libs/enums/order.enum";
+import { OrderInquiry } from "../libs/types/order";
 
 const orderService = new OrderService();
 
@@ -20,6 +21,28 @@ orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standart.code).json(Errors.standart);
   }
-};
+}
+
+
+orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
+    try {
+      console.log("getMyOrders");
+      const { page, limit, orderStatus } = req.query;
+      const inquiry: OrderInquiry = {
+        page: Number(page),
+        limit: Number(limit),
+        orderStatus: orderStatus as OrderStatus,
+      };
+      console.log("inquiry:", inquiry);
+      const result = await orderService.getMyOrders(req.member, inquiry);
+
+      res.status(HttpCode.CREATED).json(result);
+    } catch (err) {
+      console.log("Error, getMyOrders:", err);
+      if (err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standart.code).json(Errors.standart);
+    }
+  };
+
 
 export default orderController;
